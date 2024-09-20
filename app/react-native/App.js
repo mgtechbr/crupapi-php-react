3b9137678fa9c251e647cd7ab367d9de8b4d0475
guilherme <guilherme-b-damasio@estudante.sesisenai.org.br>
2024-09-20 14:55:09 -0300

    alterando app para react=native padroa

--- app/react-native/App.js
+++ app/react-native/App.js
@@ -1,15 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+import { StyleSheet, Text, View } from 'react-native';
 
-import UserContextProvider from './src/contexts/UserContext';
-import MainStack from './src/stacks/MainStack';
-
-export default () => {
+export default function App() {
   return (
-    <UserContextProvider>
-      <NavigationContainer>
-        <MainStack />
-      </NavigationContainer>
-    </UserContextProvider>
+    <View style={styles.container}>
+      <Text>Foi porra</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
